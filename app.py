a8b3c032ef9fa5c38e69984fede4c36030c1a26b
Hemant Karekar <hemant.v.karekar@gmail.com>
2021-09-20 14:23:40 +0530

    Initial Template
    Code Block

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html");
+
+@app.route("/exam")
+def exam():
+    return "<p>Hello, Exam!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
