a90d63d4047e02e1bbc49321a6ecb73f7b032b1d
Hemant Karekar <hemant.v.karekar@gmail.com>
2022-01-11 00:44:07 +0530

    html quick template

--- app.py
+++ app.py
@@ -1,36 +1,9 @@
 from datetime import datetime
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
-from flask_sqlalchemy import SQLAlchemy
 from admin import admin
 from actors import student, faculty
 
 app = Flask(__name__)
-
-""" DATABASE """
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'test'
- 
-mysql = MySQL(app)
- 
-#Creating a connection cursor
-cursor = mysql.connection.cursor()
- 
-#Executing SQL Statements
-cursor.execute(''' CREATE TABLE testdb(name, downloads) ''')
-cursor.execute(''' INSERT INTO testdb VALUES(Hemant,12) ''')
- 
-#Saving the Actions performed on the DB
-mysql.connection.commit()
- 
-#Closing the cursor
-cursor.close()
-
-app.register_blueprint(admin)
-app.register_blueprint(student)
-app.register_blueprint(faculty)
 
 """ DATE """
 @app.context_processor
@@ -47,21 +20,21 @@ def not_found(e):
 def index():
     return render_template("index.html");
 
-@app.route("/features")
-def features():
-    return render_template("comingsoon.html")
-@app.route("/pricing")
-def pricing():
-    return render_template("comingsoon.html")
-@app.route("/blogs")
-def blogs():
-    return render_template("comingsoon.html")
-@app.route("/login")
-def login():
-    return render_template("comingsoon.html")
-@app.route("/register")
-def register():
-    return render_template("comingsoon.html")
+# @app.route("/features")
+# def features():
+#     return render_template("comingsoon.html")
+# @app.route("/pricing")
+# def pricing():
+#     return render_template("comingsoon.html")
+# @app.route("/blogs")
+# def blogs():
+#     return render_template("comingsoon.html")
+# @app.route("/login")
+# def login():
+#     return render_template("comingsoon.html")
+# @app.route("/register")
+# def register():
+#     return render_template("comingsoon.html")
 
 @app.route("/student/<username>")
 @app.route("/faculty/<username>")
